2a55b6a640d64e39ef7db977d9735b4a95198652
Erin Strickland <stricklande93@gmail.com>
2018-04-25 17:20:34 -0700

    Init commit

---
+++ src/core.js
@@ -0,0 +1,11 @@
+import {List, Map} from 'immutable'
+
+export const setEntries = (state, entries) => state.set('entries', List(entries))
+
+export const next = (state) => {
+  const entries = state.get('entries')
+  return state.merge({
+    vote: Map({pair: entries.take(2)}),
+    entries: entries.skip(2)
+  })
+}
